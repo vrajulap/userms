4ba21c95e319db49452282768676b90d243e01ea
Venkateswara Rajulapati <VenkateshwarRao.Rajulapati@nrg.com>
2023-03-02 14:56:47 +0530

    initial commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into user (name) values ('AbdulRahman');
-insert into user (name) values ('Bala');
+insert into user (name) values ('Loukya');
+insert into user (name) values ('Bhavani');
 insert into user (name) values ('Venkatesh');
-insert into user (name) values ('Preeti');
+insert into user (name) values ('Praneeth');
